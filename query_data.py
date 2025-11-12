a710361d050e9020a601e17456401e83ce316a66
Khang Nguyen <dsy1090@wmich.edu>
2025-11-11 20:50:54 -0500

    fix naming convention and comments

--- query_data.py
+++ query_data.py
@@ -28,44 +28,49 @@ AGAIN, THE MOST IMPORTANT THINGS ARE: ANSWER IN LESS THAN 190 CHARACTERS, IN SWA
 
 def main():
     # grab query for chatbot from terminal
+    # usage: query_data.py "Insert the question here"
     parser = argparse.ArgumentParser()
-    parser.add_argument("query_text", type=str, help="The query text.")
+    parser.add_argument("user_question", type=str, help="The query text.")
     args = parser.parse_args()
-    query_text = args.query_text
-    query_rag(query_text)
+    user_question = args.user_question
 
+    # Query the vector database
+    query_rag(user_question)
 
-def query_rag(query_text: str):
+# make the function for other files to call as well as call from terminal with main()
+def query_rag(user_question: str):
 
     # can change the model here
-    model = "mannix/llamax3-8b-alpaca"
+    MODEL_NAME = "mannix/llamax3-8b-alpaca"
 
     # prepare the DB.
-    embedding_function = get_embedding_function(input_model= model)
+    embedding_function = get_embedding_function(input_model= MODEL_NAME)
     db = Chroma(persist_directory=DB_PATH, embedding_function=embedding_function)
 
     # search the DB.
-    results = db.similarity_search_with_score(query_text, k=3) # Modify this to grab more context per prompt, might hurt if too high or too low
+    results = db.similarity_search_with_score(user_question, k=3) # Modify this to grab more context per prompt, might hurt if too high or too low
 
-
-    # Make sure response is relevant
+    # make sure response is relevant
     if len(results) == 0 or results [0][1] < 0.7:
         print("Unable to find matching results.")
         return
 
+    # combining system prompt and user query
     context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
-    prompt = prompt_template.format(context=context_text, question=query_text)
-    # print(prompt)
+    prompt = prompt_template.format(context=context_text, question=user_question)
 
+    # give the model the full prompt
+    ollama_model = OllamaLLM(model = MODEL_NAME)
+    model_response = ollama_model.invoke(prompt)
 
-    ollama_model = OllamaLLM(model = model)
-    response_text = ollama_model.invoke(prompt)
-
+    # grab the sources and add it to the formatted response for traceability
     sources = [doc.metadata.get("id", None) for doc, _score in results]
-    formatted_response = f"\n{model}:{response_text}\n\nSources: {sources}"
+    formatted_response = f"\n{MODEL_NAME}:{model_response}\n\nSources: {sources}"
     print(formatted_response)
-    return response_text
+
+    # return the model response
+    return model_response
 
 
 if __name__ == "__main__":
